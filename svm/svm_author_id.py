c9a08447d51d0cecdaa50edd1ad0b5e3f2108593
Marcos Garcia <marcgarcia@groupon.com>
2015-05-02 22:20:15 +0100

    train only 1% of data

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -26,6 +26,48 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn import svm
+
+
+print "Train only 100% of data"
+print "--------------------"
+
+clf = svm.SVC(kernel='linear')
+
+t0 = time()
+clf.fit(features_train, labels_train)
+
+print "training time:", round(time()-t0, 3), "s"
+
+t0 = time()
+pred = clf.predict(features_test)
+print "prediction time:", round(time()-t0, 3), "s"
+
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(labels_test, pred)
+
+print "accuracy: %s", accuracy
+
+print "Train only 1% of data"
+print "--------------------"
+
+clf = svm.SVC(kernel='linear')
+
+t0 = time()
+size = len(features_train) / 100
+
+clf.fit(features_train[:size], labels_train[:size])
+
+print "training time:", round(time()-t0, 3), "s"
+
+t0 = time()
+pred = clf.predict(features_test)
+print "prediction time:", round(time()-t0, 3), "s"
+
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(labels_test, pred)
+
+print "accuracy: %s", accuracy
 
 #########################################################
 
